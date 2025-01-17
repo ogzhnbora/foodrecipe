2f3c513d98d5bedc1f3573bbd1c9e2ba6b4ca41c
oguzhnbora <boracabuk@hotmail.com>
2025-01-17 20:19:43 +0300

    Add project demo video

--- obj/Debug/net8.0/YemekTarifiSitesi.AssemblyInfo.cs
+++ obj/Debug/net8.0/YemekTarifiSitesi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("YemekTarifiSitesi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a63dd499d2927cc3548992fa484db18883b9cfb")]
 [assembly: System.Reflection.AssemblyProductAttribute("YemekTarifiSitesi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("YemekTarifiSitesi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
